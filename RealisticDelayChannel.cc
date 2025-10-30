73bacbde81e13d9cc9033e01f671c91fec95c530
josigar <104169012+josigar123@users.noreply.github.com>
2025-10-30 17:13:26 +0100

    Update RealisticChannelDelay API
    Includes a method to dynamically calculate delay based on a  randomly selected jitter and propagation between destinations

--- RealisticDelayChannel.cc
+++ RealisticDelayChannel.cc
@@ -1,4 +1,5 @@
 #include "RealisticDelayChannel.h"
+#include "Node.h"
 
 Define_Channel(RealisticDelayChannel);
 
@@ -6,4 +7,19 @@ void RealisticDelayChannel::initialize()
 {
     cDatarateChannel::initialize();
     baseLatency = par("baseLatency");
+    jitterPercentage = par("jitterPercentage");
+    propSpeed = par("propSpeed");
+}
+
+simtime_t RealisticDelayChannel::computeDynamicDelay(cModule *src, cModule *dst){
+
+    Node *srcNode = check_and_cast<Node *>(src);
+    Node *dstNode = check_and_cast<Node *>(dst);
+    double distanceM = sqrt(pow(srcNode->x - dstNode->x, 2) + pow(srcNode->y - dstNode->y, 2));
+
+
+    double jitterMs = uniform(-jitterPercentage, jitterPercentage) * baseLatency.dbl();
+    double propagationDelayMs = (distanceM / propSpeed) * 1000;
+
+    return (simtime_t) (baseLatency + jitterMs + propagationDelayMs);
 }
--- RealisticDelayChannel.h
+++ RealisticDelayChannel.h
@@ -12,6 +12,8 @@ class RealisticDelayChannel : public cDatarateChannel
 {
   protected:
     simtime_t baseLatency;
+    double jitterPercentage;
+    double propSpeed;
 
   protected:
     virtual void initialize() override;
@@ -19,6 +21,12 @@ class RealisticDelayChannel : public cDatarateChannel
   public:
     // Make a public safe getter for the full current delay
     simtime_t getCurrentDelay() const { return getDelay() + baseLatency; }
+
+    // Just get the base latency for the link
+    simtime_t getBaseLatency() const { return baseLatency; }
+
+    // Used for calculating the dynamic delay for a link from src to dst, returns the delay as simtime_t
+    simtime_t computeDynamicDelay(cModule *src, cModule *dst);
 };
 
 #endif
